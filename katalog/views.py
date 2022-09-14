00bee585b665354ef9d05a1b7f7bbc8c75270f53
TGevinT <t.gevintaufan@gmail.com>
2022-09-14 22:19:48 +0700

    tugas point 1-3

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Teuku Gevin Taufan',
+        'student_id': '2106750194',
+    }
+    return render(request, "katalog.html", context)
